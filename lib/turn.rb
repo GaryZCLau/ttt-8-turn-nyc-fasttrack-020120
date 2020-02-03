090437efa161c9f800a5d60ad25d70b3553ba150
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-03 23:31:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,29 @@
 def display_board(board)
 
 end
+
+def valid_move?(board, index)
+
+  def position_taken? (board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else board[index] == "X" || board[index] == "O"
+      return true
+    end
+  end
+
+  def onboard(index)
+    if index.between?(0,8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (onboard(index) == true)
+    return true
+  else
+    return false
+  end
+
+end
